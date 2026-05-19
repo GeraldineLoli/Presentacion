59632129c70f6e859367ecadd1ee93eae8ee72f7
Piero Robles <paroblesv@gmail.com>
2026-05-19 16:08:02 -0500

    Implementando mi nombre

--- Presentacion.java
+++ Presentacion.java
@@ -2,5 +2,6 @@ public class Presentacion {
     public static void main(String[] args) {
         System.out.println("Integrante 1: [Nombre]");
         System.out.println("Integrante 2: [Nombre]");
+        System.out.println("integrante 4: [Piero Alonso Robles Vásquez]");
     }
 }
